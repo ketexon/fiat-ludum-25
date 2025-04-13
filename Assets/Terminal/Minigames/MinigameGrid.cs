57d44cca5322f9101bb1833c2197062b7b4f8043
ketexon <zane.a.s.clark@gmail.com>
2025-04-12 18:59:48 -0700

    HOPEFULLY!!!!!

--- Assets/Terminal/Minigames/MinigameGrid.cs
+++ Assets/Terminal/Minigames/MinigameGrid.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class MinigameGrid : MonoBehaviour
 {
     RectTransform RectTransform => (RectTransform)transform;
+
+    public Transform Container;
     
     [SerializeField] private int nRows = 6;
     [SerializeField] private int nCols = 7;
@@ -14,10 +16,16 @@ public class MinigameGrid : MonoBehaviour
     public Rect GetCellRect(Vector2Int cell)
     {
         return new Rect(
-            RectTransform.rect.x + (cell.x * CellWidth),
-            RectTransform.rect.y + (cell.y * CellHeight),
+            cell.x * CellWidth,
+            -cell.y * CellHeight,
             CellWidth,
             CellHeight
         );
+    }
+
+    public void MoveTo(RectTransform t, Vector2Int cell)
+    {
+        t.anchoredPosition = GetCellRect(cell).position;
+        Debug.Log(GetCellRect(cell).position);
     }
 }
