c8359003790d7ff6dd3026cb0d47dc5581a9ca29
Rachel Jin <racheljin24@gmail.com>
2025-04-12 18:22:40 -0700

    dobs

---
+++ Assets/Terminal/Commands/AboutCommand.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class AboutCommand : ShellCommand
+{
+    public override void Execute(string[] args)
+    {
+        base.Execute(args);
+        
+        Terminal.Println("KetexOS v4.12.25\n\n(c) 2125 ReNova Laboratories ReNova Laboratories is leading the next generation of humanity into space.\n Planet Promethos is our future. Your task is to locate, collect, and return high quantities of Resource X back to Earth.");
+
+    }
+}
